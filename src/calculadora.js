4f789ddbcd860b37f3ba23d2e49a3abde0ca932b
Sergio Garrido <sergiogar@payoneer.com>
2026-08-03 10:00:00 +0200

    implementada la función de suma de dos valores

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,5 +1,8 @@
 // Clase Calculadora. Las operaciones se irán implementando siguiendo BDD (red-green-refactor).
 class Calculadora {
+  sumar (a, b) {
+    return a + b
+  }
 }
 
 module.exports = Calculadora
