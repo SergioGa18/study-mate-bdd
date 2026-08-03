37555d107fd898b5c7eed5522186116861884871
Sergio Garrido <sergiogar@payoneer.com>
2026-08-03 09:40:00 +0200

    creados los pasos para la prueba del escenario suma de dos valores

---
+++ features/step_definitions/calculadora_steps.js
@@ -0,0 +1,19 @@
+const assert = require('assert')
+const { Dado, Cuando, Entonces } = require('@cucumber/cucumber')
+const Calculadora = require('../../src/calculadora')
+
+Dado('que he introducido el número {int}', function (numero) {
+  if (!this.numeros) {
+    this.numeros = []
+    this.calculadora = new Calculadora()
+  }
+  this.numeros.push(numero)
+})
+
+Cuando('pulso el botón sumar', function () {
+  this.resultado = this.calculadora.sumar(this.numeros[0], this.numeros[1])
+})
+
+Entonces('el resultado debe ser {float}', function (esperado) {
+  assert.strictEqual(this.resultado, esperado)
+})
---
+++ src/calculadora.js
@@ -0,0 +1,5 @@
+// Clase Calculadora. Las operaciones se irán implementando siguiendo BDD (red-green-refactor).
+class Calculadora {
+}
+
+module.exports = Calculadora
